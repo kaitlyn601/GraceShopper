166fba4356cbb14d3aea3a4cc7be84ddccd8d333
Tedi Lowney <tmlowney1@gmail.com>
2022-03-03 16:39:50 -0500

    Add to Cart button handler uses local storage to check for an existing cart, or else creates a guest cart, and adds item

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -2,11 +2,58 @@ import React from "react";
 import { connect } from "react-redux";
 import { getProduct } from "../store/product";
 import EditProduct from "./EditProduct";
+// added on branch feature/add-to-cart-button :
+import { getCart } from "../store/cart";
 
 class SingleProduct extends React.Component {
+  constructor() {
+    super();
+    this.handleAddToCart = this.handleAddToCart.bind(this);
+  }
   componentDidMount() {
     this.props.loadSingleProduct(this.props.match.params.id);
   }
+
+  handleAddToCart() {
+    const { product } = this.props;
+    const { cart } = this.props;
+    const { isLoggedIn } = this.props;
+    if (isLoggedIn) {
+      // USE CART ON STATE
+    } else {
+      // USE LOCAL STORAGE
+      let orderItemObj = {
+        price: product.price,
+        quantity: 1,
+        productId: product.id,
+        // orderId:  No orderId until user account (and a cart) are created in DB
+        // id:  an id property will be created by Sequelize automatically, when order-item is instantiated in DB via "OrderItem.create()"
+      };
+      let guestCartArray;
+
+      let guestCart = window.localStorage.getItem("cart");
+      if (guestCart) {
+        console.log("we do have a cart", guestCart);
+        guestCartArray = JSON.parse(guestCart);
+      } else {
+        guestCartArray = [];
+      }
+      guestCartArray.push(orderItemObj);
+      let stringifiedCartArray = JSON.stringify(guestCartArray);
+      window.localStorage.setItem("cart", stringifiedCartArray);
+    }
+  }
+  // if user is logged in (isLoggedIn), then the add-to-cart handler
+  // will add the items to the cart on state (an array)
+  // if user is NOT logged in, instead handler will add items to local storage
+  // this entails:
+
+  // IF USER IS LOGGED IN  (isLoggedIn), then the add-to-cart handler will:
+  // build up a similar object (same shape)
+  // except its property .orderId will be this.props.state.cart.id
+  // and instead of adding this orderItemObject to an array that gets stringified and stored in localStorage,
+  // we'll use a THUNK to create instance of OrderItem in DB, using this object, and also update Redux store
+
   render() {
     const { product } = this.props;
     if (!product.id) return <div>Loading</div>;
@@ -26,7 +73,9 @@ class SingleProduct extends React.Component {
               <button>+</button>
             </div>
 
-            <button>ADD TO CART</button>
+            <button type="button" onClick={this.handleAddToCart}>
+              ADD TO CART
+            </button>
           </div>
         </div>
         <div className="description-container">
@@ -51,12 +100,18 @@ const mapState = (state) => {
   return {
     product: state.product,
     isAdmin: state.auth.isAdmin,
+    // added on branch feature/add-to-cart-button :
+    isLoggedIn: !!state.auth.id,
+    userId: state.auth.id,
+    cart: state.cart,
   };
 };
 
 const mapDispatch = (dispatch) => {
   return {
     loadSingleProduct: (id) => dispatch(getProduct(id)),
+    // added on branch feature/add-to-cart-button :
+    getCart: (id) => dispatch(getCart(id)),
   };
 };
 
