facce762f961ea96c1bdc1ad4cec9e9366505ba8
kaitlyn601 <kaitlynzou410@gmail.com>
2022-03-04 17:44:20 -0500

    added singleUser order history

--- client/components/SingleUser.js
+++ client/components/SingleUser.js
@@ -8,11 +8,39 @@ class SingleUser extends React.Component {
   }
   render() {
     const { user } = this.props;
+    const { orders } = user;
+    let fulfilledOrders = [];
+    if (orders) {
+      fulfilledOrders = orders.filter((order) => order.fulfilled);
+    }
+    console.log("fulfilled", fulfilledOrders);
+    console.log("props", this.props.user);
     if (!user.id) return <div>Loading</div>;
     return (
       <div>
         <h3>Username {user.username}</h3>
         <h3>User Type: {user.isAdmin ? "Admin" : "User"}</h3>
+        <h3>Past Order:</h3>
+        {fulfilledOrders.length < 1
+          ? "There are currently no orders"
+          : fulfilledOrders.map((order) => {
+              return (
+                <div key={order.id}>
+                  <div>
+                    <h3>Order Id: {order.id}</h3>
+                    {order.orderitems.map((orderItem) => {
+                      return (
+                        <div key={orderItem.id}>
+                          <h3>Order Item ID: {orderItem.id}</h3>
+                          <h3>Quantity: {orderItem.quantity}</h3>
+                          <h3>Price: {orderItem.price}</h3>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
       </div>
     );
   }
