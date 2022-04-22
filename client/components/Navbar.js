d593bb30cb9b48c5f8779fdf1a9d2c018396fc79
kaitlyn601 <kaitlynzou410@gmail.com>
2022-04-22 02:57:18 -0400

    fixed floating footer

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,69 +1,61 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { logout } from "../store";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { logout } from '../store';
 
 const Navbar = ({ handleClick, isLoggedIn, isAdmin }) => (
-  <header className="header">
-    <Link to="/home">
+  <header className='header'>
+    <Link to='/home'>
       <img
-        className="logo-img"
-        alt="logo"
-        src="https://cdn.shopify.com/s/files/1/0012/8660/2848/files/logo-horizontal_190x.png?v=1546919365"
+        className='logo-img'
+        alt='logo'
+        src='https://cdn.shopify.com/s/files/1/0012/8660/2848/files/logo-horizontal_190x.png?v=1546919365'
       />
     </Link>
-    <nav className="main-nav">
+    <nav className='main-nav'>
       {isLoggedIn ? (
-        <ul className="main-nav-list">
+        <ul className='main-nav-list'>
           {/* The navbar will show these links after you log in */}
           <li>
-            <Link className="main-nav-link" to="/home">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link className="main-nav-link" to="/products">
-              Chocolate
-            </Link>
-          </li>
-          <li>
-            <Link className="main-nav-link" to="/cart">
-              <span className="material-icons-outlined">shopping_bag</span>
+            <Link className='main-nav-link' to='/products'>
+              Shop
             </Link>
           </li>
           {isAdmin ? (
             <li>
-              <Link className="main-nav-link" to="/users">
+              <Link className='main-nav-link' to='/users'>
                 Users Info
               </Link>
             </li>
           ) : (
-            ""
+            ''
           )}
           <li>
-            <a className="main-nav-link" href="#" onClick={handleClick}>
+            <Link className='main-nav-link' to='/cart'>
+              <span className='material-icons-outlined'>shopping_bag</span>
+            </Link>
+          </li>
+          <li>
+            <a className='main-nav-link' href='#' onClick={handleClick}>
               Logout
             </a>
           </li>
         </ul>
       ) : (
-        <ul className="main-nav-list">
+        <ul className='main-nav-list'>
           {/* The navbar will show these links before you log in */}
-          <Link className="main-nav-link" to="/home">
-            Home
+          <Link className='main-nav-link' to='/products'>
+            Shop
           </Link>
-          <Link className="main-nav-link" to="/products">
-            Chocolate
-          </Link>
-          <Link className="main-nav-link cart-logo" to="/cart">
-            <span id="cart" className="material-icons-outlined">
+          <Link className='main-nav-link cart-logo' to='/cart'>
+            <span id='cart' className='material-icons-outlined'>
               shopping_bag
             </span>
           </Link>
-          <Link className="main-nav-link" to="/login">
+          <Link className='main-nav-link' to='/login'>
             Login
           </Link>
-          <Link className="main-nav-link" to="/signup">
+          <Link className='main-nav-link' to='/signup'>
             Sign Up
           </Link>
         </ul>
